321e63b6de0058e5b1e1664708037d87f193bca8
soma0sd <soma0sd@gmail.com>
2018-05-02 20:15:19 +0900

    테스트용 템플릿 업로드

---
+++ index.md
@@ -0,0 +1,7 @@
+---
+title: 프로그래밍 기초
+layout: book
+---
+
+## UPDATE
+* **2018/5/2** 교재용 Jekyll 템플릿 테스트 시작
---
+++ js/functions.js
@@ -0,0 +1,91 @@
+/*
+██    ██  █████  ██████  ███████
+██    ██ ██   ██ ██   ██ ██
+██    ██ ███████ ██████  ███████
+ ██  ██  ██   ██ ██   ██      ██
+  ████   ██   ██ ██   ██ ███████
+*/
+var viewposrtSmall =   600;
+var viewposrtMedium =  960;
+var viewposrtLarge =  1280;
+var viewposrtXLarge = 1920;
+
+var pathname = window.location.pathname;
+
+var $nav = $("nav");
+var $navBtn = $("#nav-open-btn");
+var $navOverlay = $("#nav-overlay");
+var $toc = $("#toc-wrap");
+var $docWrap = $("#doc-wrap");
+
+/*
+███████ ██    ██ ███    ██  ██████
+██      ██    ██ ████   ██ ██
+█████   ██    ██ ██ ██  ██ ██
+██      ██    ██ ██  ██ ██ ██
+██       ██████  ██   ████  ██████
+*/
+function hideSideNav(vel = "fast"){
+  let sidebarWidth = $nav.width();
+  $nav.animate({left: -sidebarWidth + "px"}, vel);
+  $navOverlay.fadeOut(vel);
+}
+function showSideNav(vel = "fast"){
+  $nav.animate({left: "0"}, vel);
+  if($(window).width() < viewposrtMedium){
+    $navOverlay.fadeIn(vel);
+  }
+}
+function SelMenuNow(){
+  if (pathname == "/"){return null}
+  let output = null;
+  $toc.find("a").each(function(i, elem){
+    if (pathname == $(this).attr("href")) {
+      $(this).addClass("active");
+      output = $(this);
+    } else {
+      $(this).removeClass("active");
+    }
+  });
+  return output;
+}
+
+/*
+███████ ██    ██ ███████ ███    ██ ████████
+██      ██    ██ ██      ████   ██    ██
+█████   ██    ██ █████   ██ ██  ██    ██
+██       ██  ██  ██      ██  ██ ██    ██
+███████   ████   ███████ ██   ████    ██
+*/
+$navOverlay.click(function(){hideSideNav()});
+$navBtn.click(function(){showSideNav()});
+
+
+/*
+██ ███    ██ ██ ████████
+██ ████   ██ ██    ██
+██ ██ ██  ██ ██    ██
+██ ██  ██ ██ ██    ██
+██ ██   ████ ██    ██
+*/
+var $nowDoc = null;
+$(document).ready(function(){
+  $nowDoc = SelMenuNow();
+  if ($nowDoc){$("#doc-title").html($nowDoc.html());}
+  if($(window).width() < viewposrtMedium){
+    hideSideNav(0);
+    $navOverlay.css("opacity", 1);
+    $nav.css("opacity", 1);
+  }
+  $(".scrollbar-inner").scrollbar();
+});
+
+$(window).resize(function(){
+  if($(window).width() < viewposrtMedium){
+    hideSideNav(0);
+    $navOverlay.css("opacity", 1);
+    $nav.css("opacity", 1);
+  } else {
+    showSideNav(0);
+  }
+});
